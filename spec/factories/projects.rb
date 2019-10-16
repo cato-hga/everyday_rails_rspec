4221b8c55c8b4f4e90090391dc4802216414e2ca
John Cato <johncato@Johns-MBP.home>
2019-10-16 16:37:58 -0400

    implemented traits and callbacks.

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -5,25 +5,19 @@ FactoryBot.define do
     due_on 1.week.from_now
     association :owner
   end
+    trait :with_notes do
+      #Callbacks let you perform additional actions on a factory-generated object before it is created, or after it is created, built, or stubbed.
+      after(:create) { |project| create_list(:note, 5, project: project) }
+    end
+    trait :due_yesterday do 
+      due_on 1.day.ago
+    end
 
-  factory :project_due_yesterday, class: Project do 
-    sequence(:name) { |n|  "Test Project #{n}" }
-    description "Sample project for testing purposes"
-    due_on 1.day.ago
-    association :owner
-  end
+    trait :due_today do
+      due_on Date.current.in_time_zone
+    end
 
-  factory :project_due_today, class: Project do
-    sequence(:name) { |n| "Test Project #{n}" }
-    description "Sample project for testing purposes"
-    due_on Date.current.in_time_zone
-    association :owner
-  end
-
-  factory :project_due_tomorrow, class: Project do
-    sequence(:name) { |n| "Test Project #{n}" }
-    description "Sample project for testing purposes"
-    due_on 1.day.from_now
-    association :owner
-  end
+    trait :due_tomorrow do
+      due_on 1.day.from_now
+    end
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -3,19 +3,24 @@ require 'rails_helper'
 RSpec.describe Project, type: :model do
   describe "late status" do
     it "is late when the due date is past today" do
-      project = FactoryBot.create(:project_due_yesterday)
+      project = FactoryBot.create(:project, :due_yesterday)
       expect(project).to be_late
     end
 
     it "is on time when the due date is today" do
-      project = FactoryBot.create(:project_due_today)
+      project = FactoryBot.create(:project, :due_today)
       expect(project).to_not be_late
     end
 
     it "is on time when the due date is in the future" do
-      project = FactoryBot.create(:project_due_tomorrow)
+      project = FactoryBot.create(:project, :due_tomorrow)
       expect(project).to_not be_late
     end
+  end
+  
+  it "can have many notes" do
+    project = FactoryBot.create(:project, :with_notes)
+    expect(project.notes.length).to eq 5
   end
   it "does not allow duplicate project names per user" do
     user = User.create(

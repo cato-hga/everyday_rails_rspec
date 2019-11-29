587f1b3c1ab2febe50ade3f6107ef038d745c44e
John Cato <jchristopher.cato@gmail.com>
2019-11-29 13:54:16 -0500

    Adds tests for update and destroy controller actions.

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -103,5 +103,109 @@ RSpec.describe ProjectsController, type: :controller do
         expect(@project.reload.name).to eq "New Project Name"
       end
     end
+
+    context "as an unauthorized user" do
+      before do
+        @user = FactoryBot.create(:user)
+        other_user = FactoryBot.create(:user)
+        @project = FactoryBot.create(:project,
+          owner: other_user,
+          name: "Same Old Name")
+      end
+
+      it "does not update project" do
+        #setup
+        project_params = FactoryBot.attributes_for(:project,
+          name: "New Name")
+        #exercise
+        sign_in @user
+        patch :update, params: { id: @project.id, project: project_params }
+        #verify
+        expect(@project.reload.name).to eq "Same Old Name"
+      end 
+
+      it "redirects to the dashboard" do
+        project_params = FactoryBot.attributes_for(:project)
+        sign_in @user
+        patch :update, params:  { id: @project.id, project: project_params }
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context "as a guest" do
+      before do
+        @project = FactoryBot.create(:project)
+      end
+
+      it "returns a 302 response" do
+        project_params = FactoryBot.attributes_for(:project)
+        patch :update, params: { id: @project.id, project: project_params }
+        expect(response).to have_http_status "302"  
+      end
+
+      it "redirects to the sign-in page" do
+        project_params = FactoryBot.attributes_for(:project)
+        patch :update, params: { id: @project.id, project: project_params }
+        expect(response).to redirect_to "/users/sign_in" 
+      end
+    end
+  end
+  describe "#destroy" do
+    context "as an authorized user" do
+      before do
+        @user = FactoryBot.create(:user)
+        @project = FactoryBot.create(:project, owner: @user)
+      end
+
+      it "destroys a project" do
+        sign_in @user
+        expect {
+          delete :destroy, params: { id: @project.id}
+        }.to change(@user.projects, :count).by(-1)
+      end
+    end
+
+    context "as an unauthorized user" do
+      before do
+        @user = FactoryBot.create(:user)
+        other_user = FactoryBot.create(:user)
+        @project = FactoryBot.create(:project, owner: other_user)
+      end
+
+      it "does not destroy the project" do
+        sign_in @user
+        expect {
+          delete :destroy, params: { id: @project.id }
+        }.to_not change(Project, :count)
+      end
+
+      it "redirects to the dashboard" do
+        sign_in @user
+        delete :destroy, params: { id: @project.id }
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context "as a guest" do
+      before do
+        @project = FactoryBot.create(:project)
+      end
+
+      it "returns a 302 response" do
+        delete :destroy, params: { id: @project.id }
+        expect(response).to have_http_status "302"
+      end
+
+      it "redirects to the sign-in page" do
+        delete :destroy, params: { id: @project.id }
+        expect(response).to redirect_to "/users/sign_in"
+      end
+
+      it "does not destroy the project" do
+        expect {
+          delete :destroy, params: { id: @project.id }
+        }.to_not change(Project, :count)
+      end
+    end
   end
 end
